85d7d7e6286e9e12236dcf456f68bd0051ee1b64
Lonely7th <35439521+Lonely7th@users.noreply.github.com>
2018-09-13 23:20:07 +0800

    init

--- bean/RiskBean.py
+++ bean/RiskBean.py
@@ -5,3 +5,10 @@ __title__ = ''
 __author__ = 'JN Zhang'
 __mtime__ = '2018/9/13 0013'
 """
+
+
+class RiskBean(object):
+    def __init__(self, min_risk, max_risk, probability):
+        self.min_risk = min_risk
+        self.max_risk = max_risk
+        self.probability = probability
---
+++ core/Cls_A.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+__title__ = ''
+__author__ = 'JN Zhang'
+__mtime__ = '2018/9/13 0013'
+"""
+
+
+if __name__ == "__main__":
+    print("hello world")
+    pass
